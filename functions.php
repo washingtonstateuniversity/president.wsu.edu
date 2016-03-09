e5bbe16dd624e9d87957bcad55c8f679e7d1f443
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2016-03-09 15:57:08 -0800

    Initial upload of president theme
    Initial upload of president theme

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+
+add_action( 'wsu_register_inline_svg', 'register_svgs' );
+function register_svgs() {
+    ob_start();
+    ?><svg><!-- SVG 1 is pasted here --></svg><?php
+    $svg_1 = ob_get_contents();
+    ob_end_clean();
+
+    ob_start();
+    ?><svg><!-- SVG 2 is pasted here --></svg><?php
+    $svg_2 = ob_get_contents();
+    ob_end_clean();
+
+    // SVG is registered with an ID of "my-inline-svg"
+    wsu_register_inline_svg( 'my-svg-1', $svg_1 );
+    wsu_register_inline_svg( 'my-svg-2', $svg_2 );
+}
