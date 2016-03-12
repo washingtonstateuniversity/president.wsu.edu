af60196a8de631e2926153a0125aa9e4f647fb29
jeremyfelt <jeremy.felt@gmail.com>
2016-03-11 16:16:21 -0800

    Update message in search box, hide when focused

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,13 @@
 (function($){
+    var $search_field = $('.search-field');
+
     $('.president-trigger-search').on('click', function() {
         $('.president-primary-search').toggle();
         $('.main-header').toggleClass('president-search-open');
     });
+
+    var placeholder = $search_field.attr('placeholder');
+
+    $search_field.on('focus', function() { $(this).attr('placeholder',''); } );
+    $search_field.on('blur', function() { $(this).attr('placeholder', placeholder ); } );
 }(jQuery));
