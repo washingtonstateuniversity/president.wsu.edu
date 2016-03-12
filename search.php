f87b3027ed9ee4e8453e3a915b9df152a309f8a9
jeremyfelt <jeremy.felt@gmail.com>
2016-03-11 16:32:33 -0800

    Add handling for no results found, show search bar on search results

--- search.php
+++ search.php
@@ -9,38 +9,61 @@ get_header();
 		<?php
 
 		get_template_part( 'parts/headers' );
-		?>
-		<section class="row single gutter pad-ends">
-			<div class="column one">
-				<h1 class="search-results-head">Search Results for <span class="search-term"><?php echo get_search_query(); ?></span></h1>
-			</div>
-		</section>
-		<?php
-		get_template_part( 'parts/archive-layout', get_post_type() );
 
-		/* @type WP_Query $wp_query */
-		global $wp_query;
+		if ( have_posts() ) {
 
-		$big = 99164;
-		$args = array(
-			'base'         => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-			'format'       => 'page/%#%',
-			'total'        => $wp_query->max_num_pages, // Provide the number of pages this query expects to fill.
-			'current'      => max( 1, get_query_var( 'paged' ) ), // Provide either 1 or the page number we're on.
-		);
-		?>
-		<footer class="main-footer archive-footer">
-			<section class="row side-right pager prevnext gutter">
+			?>
+			<section class="row single gutter pad-ends">
 				<div class="column one">
-					<?php echo paginate_links( $args ); ?>
+					<h1 class="search-results-head">Search Results for <span class="search-term"><?php echo get_search_query(); ?></span></h1>
 				</div>
-				<div class="column two">
-					<!-- intentionally empty -->
-				</div>
-			</section><!--pager-->
-		</footer>
+			</section>
+			<?php
 
-		<?php get_template_part( 'parts/footers' ); ?>
+			get_template_part( 'parts/archive-layout', get_post_type() );
+
+			/* @type WP_Query $wp_query */
+			global $wp_query;
+
+			$big  = 99164;
+			$args = array(
+				'base'    => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+				'format'  => 'page/%#%',
+				'total'   => $wp_query->max_num_pages, // Provide the number of pages this query expects to fill.
+				'current' => max( 1, get_query_var( 'paged' ) ), // Provide either 1 or the page number we're on.
+			);
+			?>
+			<footer class="main-footer archive-footer">
+				<section class="row side-right pager prevnext gutter">
+					<div class="column one">
+						<?php echo paginate_links( $args ); ?>
+					</div>
+					<div class="column two">
+						<!-- intentionally empty -->
+					</div>
+				</section><!--pager-->
+			</footer>
+			<?php get_template_part( 'parts/footers' );
+		} else {
+			?>
+			<section class="row single gutter pad-ends">
+				<div class="column one">
+					<h1 class="search-results-head">No results found for <span class="search-term"><?php echo get_search_query(); ?></span></h1>
+				</div>
+			</section>
+			<section class="row side-right gutter pad-ends">
+
+				<div class="column one">
+					<p>Sorry, but nothing matched your search terms. Please try again with some different keywords.</p>
+
+				</div><!--/column-->
+
+				<div class="column two"></div><!--/column two-->
+
+			</section>
+			<?php
+		}
+		?>
 
 	</main>
 <?php
