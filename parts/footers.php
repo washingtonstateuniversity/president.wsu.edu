dd558e847769cb5e50ccef3a13e835eedc32b8e0
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2016-03-17 11:46:32 -0700

    0.0.13
    * Addition of Foundation graphs
    * Splits shift revenue source charts into two for responsiveness
    * Initial responsive work
    * Adds a wrap to the footer back to top so that it can position the
    same as the search button

--- parts/footers.php
+++ parts/footers.php
@@ -6,7 +6,9 @@
 			Mobile: 509-432-5372
 		</p>
 	</div>
-	<div class="column two btt-space">
-		<div class="btt"><p><a href="#top">^</a></p></div>
+	<div class="btt-wrap"> <!-- The wrap positions the button to equal distance of the search icon -->
+		<div class="column two btt-space">
+			<div class="btt"><p><a href="#top">^</a></p></div>
+		</div>
 	</div>
 </footer>
